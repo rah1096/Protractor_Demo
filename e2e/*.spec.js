de0d04cc2da1c5532b3eb047cf3d35848799b797
Tim <chent1101@gmail.com>
2015-08-04 22:01:53 -0600

    second test

--- e2e/*.spec.js
+++ e2e/*.spec.js
@@ -1,16 +1,57 @@
 describe('Protractor Demo App', function() {
-   it('should add one and two', function() {
-       browser.get('http://juliemr.github.io/protractor-demo/');
+    var firstNumber = element(by.model('first'));
+    var secondNumber = element(by.model('second'));
+    var operator = element(by.model('operator'));
+    var gobutton = element(by.id('gobutton'));
+    var latestResult = element(by.binding('latest'));
 
-       element(by.model('first')).sendKeys(10);
-       element(by.model('second')).sendKeys(2);
-       element(by.model('operator')).sendKeys('*');
+    beforeEach(function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+    });
 
-       element(by.id('gobutton')).click();
+    it('should have a title', function() {
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    });
 
-       expect(element(by.binding('result.value')).getText()).
-           toEqual('20');
-   });
+    it('should add one and two', function() {
+       firstNumber.sendKeys(1);
+       secondNumber.sendKeys(2);
+
+       gobutton.click();
+
+       expect(latestResult.getText()).toEqual('3');
+    });
+
+    it('should add four and six', function() {
+        firstNumber.sendKeys(4);
+        secondNumber.sendKeys(6);
+
+        gobutton.click();
+
+        expect(latestResult.getText()).toEqual('10')
+    });
+
+    it('should multiply four and five', function() {
+       firstNumber.sendKeys(4);
+       secondNumber.sendKeys(5);
+
+       operator.sendKeys('*');
+
+       gobutton.click();
+
+       expect(latestResult.getText()).toEqual('21'); //fail
+    });
+
+    it('should divide thirty and five', function() {
+       firstNumber.sendKeys(30);
+       secondNumber.sendKeys(5);
+
+       operator.sendKeys('/');
+
+       gobutton.click();
+
+       expect(latestResult.getText()).toEqual('6');
+    });
 });
 
 
