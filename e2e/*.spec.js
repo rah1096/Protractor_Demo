65f4d6c924c93481b51b9d15c0b550755b5b54ba
Tim <chent1101@gmail.com>
2015-08-04 22:49:03 -0600

    third test, lists of elements

--- e2e/*.spec.js
+++ e2e/*.spec.js
@@ -4,54 +4,28 @@ describe('Protractor Demo App', function() {
     var operator = element(by.model('operator'));
     var gobutton = element(by.id('gobutton'));
     var latestResult = element(by.binding('latest'));
+    var history = element.all(by.repeater('result in memory'));
 
     beforeEach(function() {
         browser.get('http://juliemr.github.io/protractor-demo/');
     });
 
-    it('should have a title', function() {
-        expect(browser.getTitle()).toEqual('Super Calculator');
-    });
-
-    it('should add one and two', function() {
-       firstNumber.sendKeys(1);
-       secondNumber.sendKeys(2);
-
-       gobutton.click();
-
-       expect(latestResult.getText()).toEqual('3');
-    });
-
-    it('should add four and six', function() {
-        firstNumber.sendKeys(4);
-        secondNumber.sendKeys(6);
-
+    function compute(a, b, operate) {
+        firstNumber.sendKeys(a);
+        secondNumber.sendKeys(b);
+        operator.sendKeys(operate);
         gobutton.click();
+    }
 
-        expect(latestResult.getText()).toEqual('10')
+    it('should have a history', function() {
+        compute(2, 4, '*');
+        compute(5, 7, '+');
+
+        expect(history.last().getText()).toContain('2 * 4');
+        expect(history.first().getText()).toContain('5 + 7');//this is wrong
+
     });
 
-    it('should multiply four and five', function() {
-       firstNumber.sendKeys(4);
-       secondNumber.sendKeys(5);
-
-       operator.sendKeys('*');
-
-       gobutton.click();
-
-       expect(latestResult.getText()).toEqual('21'); //fail
-    });
-
-    it('should divide thirty and five', function() {
-       firstNumber.sendKeys(30);
-       secondNumber.sendKeys(5);
-
-       operator.sendKeys('/');
-
-       gobutton.click();
-
-       expect(latestResult.getText()).toEqual('6');
-    });
 });
 
 
--- e2e/conf.js
+++ e2e/conf.js
@@ -1,4 +1,9 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['*.spec.js']
+    specs: ['*.spec.js'],
+    multiCapabilities: [{
+        browserName: 'firefox'
+    }, {
+        browserName: 'chrome'
+    }]
 };
